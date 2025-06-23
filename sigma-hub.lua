6e62c74b40cf3c85308d2779b387a53e45eedd66
Bot418 <arkhanarahman@gmail.com>
2025-06-23 11:12:14 +0700

    upload sigma hub script
    add sigma-hub.lua

---
+++ sigma-hub.lua
@@ -0,0 +1,25 @@
+-- Sigma Hub V3 (Remake)
+-- Made by [Nama Kamu]
+
+-- Load Infinite Yield
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+
+-- Simple GUI
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(0, 200, 0, 100)
+Frame.Position = UDim2.new(0.5, -100, 0.5, -50)
+Frame.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+Frame.BorderSizePixel = 0
+
+TextLabel.Parent = Frame
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Text = "Sigma Hub Loaded!"
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.BackgroundTransparency = 1
+
+-- Tambahkan fitur lainnya di bawah ini
